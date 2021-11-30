22a785f3d8d3507fd584660183c656a90b543d8c
Benny Luo <bennyluo95@gmail.com>
2021-11-29 22:16:14 -0500

    renamed folders

---
+++ frontend - part8/src/components/Authors.js
@@ -0,0 +1,78 @@
+  
+import React, {useState} from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+
+import { ALL_AUTHORS, ADD_BORN } from '../queries'
+
+const Authors = (props) => {
+  const [name, setName] = useState("")
+  const [born, setBorn] = useState("")
+  const result = useQuery(ALL_AUTHORS)
+  const [addBorn] = useMutation(ADD_BORN, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+
+  if (!props.show) {
+    return null
+  }
+   
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  const authors= result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+    console.log(name,born);
+    addBorn({variables:{name, setBornTo: Number(born)}})
+    
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <h3>Set Birth Year</h3>
+      <div>
+      <form onSubmit={submit} >
+        <select onChange={({ target }) => setName(target.value)}>
+          {authors.map( (a, idx) => <option value={a.name} key={idx}>{a.name}</option>)}
+        </select>
+        <div>
+          born 
+          <input
+            type='number'
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type='submit'>Update Author</button>
+      </form>
+      </div>
+    </div>
+  )
+}
+
+export default Authors
---
+++ frontend - part8/src/queries.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+    query {
+        allAuthors {
+        name 
+        born 
+        bookCount
+        }
+    }
+`
+
+export const ALL_BOOKS = gql`
+    query {
+        allBooks {
+            title
+            author
+            published
+        }
+    }
+`
+
+export const ADD_BOOK = gql`
+    mutation addBook($title: String!, $published: Int, $author: String, $genres: [String]) {
+        addBook(
+            title: $title, 
+            published: $published, 
+            author: $author, 
+            genres: $genres
+        ) {
+            title
+            published
+            author
+            id
+            genres
+        }
+  }
+`
+
+export const ADD_BORN = gql`
+  mutation EditAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
